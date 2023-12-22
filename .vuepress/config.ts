196bbb6c91f13d8089f639db61c1b55f516fe366
passwordgloo <passwordgloo@163.com>
2023-12-22 22:23:32 +0800

    修改dist位置(0.0.0.0.0.5)

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -6,7 +6,7 @@ import { themeConfig } from './config/switch';
 export default defineUserConfig({
   title: "小鹿乐园",
   description: "本地文档",
-  dest:"${sourceDir}/.vuepress/dist",
+  dest:'.vuepress/dist',
   locales: {
     // 键名是该语言所属的子路径
     // 作为特例，默认语言可以使用 '/' 作为其路径。
